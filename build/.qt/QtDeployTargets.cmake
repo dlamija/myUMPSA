bd8b2ff18c874b8a72be432b21a9020a311eb7d6
dlamija <mija@ramiza.com>
2025-04-25 20:30:54 +0800

    Remove build folder from Git tracking

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_myUMPSA_FILE /Users/dlamija/Sandbox/cursor/myUMPSA/build/myUMPSA.app/Contents/MacOS/myUMPSA)
-set(__QT_DEPLOY_TARGET_myUMPSA_TYPE EXECUTABLE)
--- build/CMakeFiles/myUMPSA.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for myUMPSA.
-# This may be replaced when dependencies are built.
